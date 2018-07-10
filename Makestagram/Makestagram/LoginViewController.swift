12b56c57aad43cb8292aa016fb1e7ba9bcd2c033
Sneha P <snehafinches@gmail.com>
2018-07-09 21:37:08 -0700

    Added Login Root View Controller/ Pink

---
+++ Makestagram/Makestagram/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  Makestagram
+//
+//  Created by Sneha Pankaj on 7/9/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
