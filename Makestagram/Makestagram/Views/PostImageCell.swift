245ffb58ccbe18e99598f43c5946691177ba3535
Sneha P <snehafinches@gmail.com>
2018-07-13 14:26:09 -0700

    posts added

---
+++ Makestagram/Makestagram/Views/PostImageCell.swift
@@ -0,0 +1,19 @@
+//
+//  PostImageCell.swift
+//  Makestagram
+//
+//  Created by Sneha Pankaj on 7/12/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PostImageCell: UITableViewCell {
+    
+    @IBOutlet weak var postImageView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+}
+}
